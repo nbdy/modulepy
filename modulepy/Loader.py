578dcae8dff547cc26e42b006cd0f64cd1a0b7c2
nbdy <pascal@eberlein.io>
2022-11-13 20:50:58 +0100

    updated method signatures and adjusted module loading function

--- modulepy/Loader.py
+++ modulepy/Loader.py
@@ -8,26 +8,31 @@ from modulepy.Base import Base, Information
 
 class Loader(object):
     @staticmethod
-    def load_module_in_directory(module_path: Path) -> Optional[Base]:
+    def load_module(module_path: Path, debug: bool = True) -> Optional[Base]:
         if not module_path.is_file() or not module_path.suffix == ".py":
             return None
         try:
-            import_path = module_path.relative_to(Path(".").absolute()).__str__().replace("/", ".").replace(".py", "")
-            module_name = module_path.name[:-3]
-            log.info(f"Attempting to load module '{module_name}' from '{import_path}'.")
-            return getattr(import_module(import_path), module_name)
+            cwd = Path(".").absolute().__str__()
+            if module_path.__str__().startswith(cwd):
+                import_path = module_path.__str__().replace(cwd, "")[1:].replace("/", ".").replace(".py", "")
+                module_name = module_path.name[:-3]
+                log.info(f"Attempting to load module '{module_name}' from '{import_path}'.")
+                return getattr(import_module(import_path, module_name), module_name)
+            else:
+                log.error(f"'{module_path}' is not in '{cwd}'")
         except Exception as e:
             log.error(f"Could not load module: {module_path}")
-            log.exception(e)
+            if debug:
+                log.exception(e)
             return None
 
     @staticmethod
-    def load_modules_in_directory(module_directory_path: Path) -> list[Base]:
+    def load_modules(module_directory_path: Path) -> list[Base]:
         r = []
         if not module_directory_path.is_dir():
             return r
         for module_path in module_directory_path.glob("*.py"):
-            m = Loader.load_module_in_directory(module_path)
+            m = Loader.load_module(module_path)
             if m is not None:
                 r.append(m)
         return r
@@ -37,4 +42,4 @@ class Loader(object):
         def matcher(module: Base) -> bool:
             return module.information.name == info.name and \
                    (ignore_version or module.information.version == info.version)
-        return any([matcher(m) for m in Loader.load_modules_in_directory(module_directory)])
+        return any([matcher(m) for m in Loader.load_modules(module_directory)])
